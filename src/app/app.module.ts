4641828dafd3c3c8fde580f1f76fa80b0cd882ff
unknown <nhussaini@luc.edu>
2019-10-25 12:13:04 -0700

    Installed angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // allows typescript to find it.
+import { MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
@@ -13,7 +15,11 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule // now angular is aware of form features
+    FormsModule,
+    BrowserAnimationsModule, // now angular is aware of form features
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
