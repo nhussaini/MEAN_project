331557d63629bb64470112919152051edd458a16
unknown <nhussaini@luc.edu>
2019-10-29 12:02:23 -0700

    added Post model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 
+import { Post } from './posts/post.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  storedPosts = [];
+  storedPosts: Post[] = []; // storedPosts is an array of type Post
 
 
-  onPostAdded(post) {
+  onPostAdded( post) {
     this.storedPosts.push(post);
   }
 }
