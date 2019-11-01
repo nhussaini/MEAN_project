68914ea8c5d973dc88fc0915e9be5354543bd1e3
unknown <nhussaini@luc.edu>
2019-10-31 19:23:06 -0700

    fetched data from the api route

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,26 @@ const express = require ('express');
 
 const app= express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
 
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id:'01a',
+      title:'First server post',
+      content:'This is coming form the server'
+    },
+    {
+      id:'02b',
+      title:'second server post',
+      content:'This is also coming form the server'
+    }
+  ];
 
-app.use((req, res, next) => {
-  res.send('Hello from express');
+  res.status(200).json({
+    message:'posts fetched successfully!',
+    posts: posts
+  });
+
 });
 
 module.exports = app;
