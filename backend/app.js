5a0feeff90e252c5e14f1038812b54ea60116102
unknown <nhussaini@luc.edu>
2019-11-06 21:01:34 -0800

    added posts with Ids

--- backend/app.js
+++ backend/app.js
@@ -29,11 +29,13 @@ app.post('/api/posts', (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  post.save();
-  console.log(post);
-  res.status(201).json({
-    message:'post added successfully'
-  })
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message:'post added successfully',
+      postId: createdPost._id
+  });
+  });
+
 });
 
 app.get('/api/posts', (req, res, next) => {
