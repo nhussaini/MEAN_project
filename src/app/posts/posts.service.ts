66d5033e554eac77fe399218100d82b9e0e34261
unknown <nhussaini@luc.edu>
2019-11-07 16:51:11 -0800

    added editing capability

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -34,6 +34,10 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
+  getPost(id: string) {
+    return {...this.posts.find(p => p.id === id)};
+  }
+
   addPost(title: string, content: string) {
     const post: Post = {id: null, title: title, content: content};
     this.http.post < {message: string, postId: string} > ('http://localhost:3000/api/posts', post)
@@ -44,6 +48,12 @@ export class PostsService {
         this.postsUpdated.next([...this.posts]);
 
     });
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = { id: id, title: title, content: content };
+    this.http.put('http://localhost:3000/api/posts/' + id, post)
+    .subscribe(response => console.log(response));
   }
 
   deletePost(postId: string) {
