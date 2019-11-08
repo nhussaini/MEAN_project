f8caf0610c747ddf59d2a44274113f761ea73961
unknown <nhussaini@luc.edu>
2019-11-07 19:46:20 -0800

    updated posts on the server

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -24,7 +24,9 @@ export class PostCreateComponent implements OnInit {
       if (paramMap.has('postId')) {
         this.mode = 'edit';
         this.postId = paramMap.get('postId');
-        this.post = this.postsService.getPost(this.postId);
+        this.postsService.getPost(this.postId).subscribe(postData => {
+          this.post = {id: postData._id, title: postData.title, content: postData.content};
+        });
       } else {
         this.mode = 'create';
         this.postId = null;
@@ -39,7 +41,7 @@ export class PostCreateComponent implements OnInit {
     if (this.mode === 'create') {
     this.postsService.addPost(form.value.title, form.value.content);
     } else {
-      this.postsService.updatePost(this.postId, form.value.title, form.value.content)
+      this.postsService.updatePost(this.postId, form.value.title, form.value.content);
     }
     form.resetForm();
   }
