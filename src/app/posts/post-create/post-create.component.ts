2080ad47b7c74133555a705991df98de468bf08e
unknown <nhussaini@luc.edu>
2019-11-08 16:07:45 -0800

    added image controls to store the image

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -25,7 +25,8 @@ export class PostCreateComponent implements OnInit {
     this.form = new FormGroup({
       title: new FormControl(null, {
         validators: [Validators.required, Validators.minLength(3)] }),
-      content: new FormControl(null, {validators: [Validators.required]})
+        content: new FormControl(null, {validators: [Validators.required]}),
+        image: new FormControl(null, {validators: [Validators.required]})
     });
     this.route.paramMap.subscribe((paramMap: ParamMap ) => {
       if (paramMap.has('postId')) {
@@ -45,6 +46,14 @@ export class PostCreateComponent implements OnInit {
         this.postId = null;
       }
     });
+  }
+
+  onImagePicked(event: Event){
+    const file = (event.target as HTMLInputElement).files[0]; // files is an array of forms
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    console.log(file);
+    console.log(this.form);
   }
 
   onSavePost() {
