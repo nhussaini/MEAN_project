94204cae9dd643a257e856113dc90f09531a7eee
unknown <nhussaini@luc.edu>
2019-11-11 15:37:26 -0800

    added pagination component

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PageEvent } from '@angular/material';
 import { Subscription } from 'rxjs';
 
 
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
+
 
 
 @Component({
@@ -19,6 +21,9 @@ export class PostListComponent implements OnInit, OnDestroy {
   // ];
   posts: Post[] = [];
   isLoading = false;
+  totalPosts = 10;
+  postsPerPage = 2;
+  pageSizeOptions = [1, 2, 5, 10];
   private postsSub: Subscription;
 
   constructor(public postsService: PostsService) {}
@@ -31,6 +36,10 @@ export class PostListComponent implements OnInit, OnDestroy {
       this.isLoading = false;
       this.posts = posts;
     });
+  }
+
+  onChangedPage(pageData: PageEvent) {
+
   }
 
   onDelete(postId: string) {
