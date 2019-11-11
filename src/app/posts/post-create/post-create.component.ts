106411681c4c04932588a28ee8d39f8c9da9bacf
unknown <nhussaini@luc.edu>
2019-11-11 15:05:44 -0800

    updated posts with images

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -37,10 +37,11 @@ export class PostCreateComponent implements OnInit {
         this.isLoading = true;
         this.postsService.getPost(this.postId).subscribe(postData => {
          this.isLoading = false;
-         this.post = {id: postData._id, title: postData.title, content: postData.content, imagePath: null};
+         this.post = {id: postData._id, title: postData.title, content: postData.content, imagePath: postData.imagePath};
          this.form.setValue({
            title: this.post.title,
-           content: this.post.content
+           content: this.post.content,
+           image: this.post.imagePath
           });
         });
       } else {
@@ -69,7 +70,12 @@ export class PostCreateComponent implements OnInit {
     if (this.mode === 'create') {
     this.postsService.addPost(this.form.value.title, this.form.value.content, this.form.value.image);
     } else {
-      this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content);
+      this.postsService.updatePost(
+        this.postId,
+        this.form.value.title,
+        this.form.value.content,
+        this.form.value.image
+        );
     }
     this.form.reset();
   }
